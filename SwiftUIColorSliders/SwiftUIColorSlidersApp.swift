226ba1aa767b3c89150ba512a3a42fd86c2be9dd
A.Vassel <102523970+AVassel@users.noreply.github.com>
2022-12-28 14:50:59 +0300

    Initial Commit

---
+++ SwiftUIColorSliders/SwiftUIColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorSlidersApp.swift
+//  SwiftUIColorSliders
+//
+//  Created by Anton Vassel on 28.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
